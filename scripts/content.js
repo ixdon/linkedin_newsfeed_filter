48b1fb185805cbf8686285cdd05617193dd4ad54
ixdon <ixdon92@gmail.com>
2026-05-02 22:46:22 +0300

    Add files via upload

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,18 @@
 console.log('LinkedIn Filter: Script injected on:', location.href);
 
 const language = document.documentElement.lang
-let keywords = ['SQL', 'Python'];
+let keywords = [];
+let stopwords = [];
 let signature_string = '';
-let fail_signature = ''
+let fail_signature = '';
 let xpath = '';
 let xpath_initial_posts = '';
 let hide_delay = 500;
+let stop_option = 'collapse';
+let ad_option = 'collapse';
+let recommend_option = 'collapse';
+let xpath_recommend = '';
+let show_more = '';
 
 //turns an array of strings into single XPath string
 function xs(L){
@@ -17,6 +23,9 @@ function xs(L){
 
 function check_footer(element){
   if(!element) return false;
+  T = element.textContent;
+  L = T.length;
+  showmore_flag = (T.slice(L-show_more.length,L) === show_more);
   return element.innerHTML.includes("www.w3.org/2000/svg");
 }
 
@@ -42,6 +51,11 @@ function fpc_descend(element){
 }
 
 function find_header(post_container){
+  const res = document.evaluate(xpath_recommend, post_container, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  const r_flag = (res.snapshotLength > 0)
+  if(r_flag){
+    return post_container.children[1].firstChild;
+  }
   let pe = post_container.firstElementChild;
   while(pe && pe.querySelectorAll(':scope > a').length !== 2){
     pe = pe.nextElementSibling; 
@@ -50,22 +64,30 @@ function find_header(post_container){
   return pe
 }
 
-function set_post_visibility(post_container, show_flag){
-  const header = find_header(post_container);
-  if(!header) return;
-  let sibling = header.nextElementSibling;
-  while(sibling){
-    sibling.style.display = show_flag ? '' : 'none';
-    sibling = sibling.nextElementSibling;
+function set_post_visibility(post_container, show_flag, mode){
+  switch(mode){
+    case "hide":
+      post_container.style.display = show_flag ? '' : 'none';
+      break;
+    case "collapse":
+      const header = find_header(post_container);
+      if(!header) return;
+      let sibling = header.nextElementSibling;
+      while(sibling){
+        sibling.style.display = show_flag ? '' : 'none';
+        sibling = sibling.nextElementSibling;
+      }
+      break;
+    default:
+      console.log('ignoring');  
   }
+
 }
 
-function highlight_post(post_container){
+function highlight_post(post_container,c){
   const header = find_header(post_container);
   if(!header) return;
-  //header.style.backgroundColor = '#CCFFCC';
-  header.style.backgroundColor = highlight_color;
-
+  header.style.backgroundColor = c;
 }
 
 function add_button(post_container){
@@ -80,7 +102,7 @@ function add_button(post_container){
   header.appendChild(btn);
 
   btn.addEventListener("click", function(){
-    set_post_visibility(post_container,btn.hiding);
+    set_post_visibility(post_container,btn.hiding,"collapse");
     btn.hiding = !btn.hiding;
     btn.textContent = btn.hiding ? '⬇️' : '⬆️';
 
@@ -91,20 +113,53 @@ function process_post(post_container){
   if(!post_container){
     return;
   }
+  const res_recommend = document.evaluate(xpath_recommend, 
+                                post_container, 
+                                null, 
+                                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, 
+                                null);
   const res = document.evaluate(xpath, post_container, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  if(res.snapshotLength){
-    set_post_visibility(post_container, false);
-    add_button(post_container);
-  }
 
+  const ad_flag = (res.snapshotLength > 0);
+  const rec_flag = (res_recommend.snapshotLength > 0)
   let highlight_flag = false;
+  let stop_flag = false;
+  if(rec_flag){
+    set_post_visibility(post_container, false, recommend_option);
+    console.log(recommend_option);
+    if(recommend_option == 'collapse') add_button(post_container);
+    return
+  }
   keywords.forEach((k) => {
     if(post_container.textContent.includes(k)) highlight_flag = true;
   });
-  if(highlight_flag) highlight_post(post_container);
+  stopwords.forEach((s) => {
+    if(post_container.textContent.includes(s)) stop_flag = true;
+  });
+  if(ad_flag){
+    set_post_visibility(post_container, false,ad_option);
+    if(stop_flag) highlight_post(post_container, stop_color);
+    if(ad_option=="collapse") add_button(post_container);
+  }
+  if(highlight_flag && stop_flag){
+    //console.log('conflict',post_container);
+    highlight_post(post_container, 'teal')
+    set_post_visibility(post_container, false,"collapse");
+    add_button(post_container);
+  };
+  if(highlight_flag && !stop_flag) {
+    highlight_post(post_container, highlight_color)
+  };
+  if(!highlight_flag && stop_flag && !ad_flag) {
+    set_post_visibility(post_container, false,stop_option);
+    highlight_post(post_container, stop_color)
+    if(stop_option=="collapse") add_button(post_container);
+  };
+
 }
 
 function main() {
+  let lastURL = location.href;
   const res = document.evaluate(xpath_initial_posts, 
                                 document, 
                                 null, 
@@ -115,9 +170,25 @@ function main() {
     if (check_footer(pc)) {
       setTimeout(() => process_post(pc), hide_delay);
     }
-  }
+  }  
   
   const observer = new MutationObserver((mutations) => {
+    if(location.href !== lastURL){
+      if(location.href.includes("/feed/")){
+        const res = document.evaluate(xpath_initial_posts, 
+                                      document, 
+                                      null, 
+                                      XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, 
+                                      null);
+        for (let i=0; i<res.snapshotLength; i++){
+          const pc = fpc_ascend(res.snapshotItem(i));
+          if (check_footer(pc)) {
+            setTimeout(() => process_post(pc), hide_delay);
+          }
+        }
+      }
+      lastURL = location.href;
+    }
     mutations.forEach((mutation) => {
       mutation.addedNodes.forEach((node) => {
         if (node.nodeType !== Node.ELEMENT_NODE) return;
@@ -150,15 +221,23 @@ fetch(fileUrl)
     }
     xpath_initial_posts = xs(data[language]["indicator"]);
     xpath = xs(data[language]["xpath"]);
+    xpath_recommend = xs(data[language]["recommend"]);
     signature_string = data[language]["footer"];
     fail_signature = data[language]["fail"];
     console.log(data[language]);
     chrome.storage.sync.get(['settings'], function(result) {
       console.log('Settings:', result.settings);
-      keywords = result?.settings?.keywords;
-      initial_hide_delay = result?.settings?.initial_hide_delay; if(!initial_hide_delay) initial_hide_delay = 2000;
-      hide_delay = result?.settings?.hide_delay; if(!hide_delay) hide_delay = 500;
-      highlight_color = result?.settings?.highlight_color; if(!highlight_color) highlight_color = "#CCFFCC";
+
+      keywords = result?.settings?.keywords || [];
+      stopwords = result?.settings?.stopwords || [];
+      initial_hide_delay = result?.settings?.initial_hide_delay || 2000;
+      hide_delay = result?.settings?.hide_delay ||  500;
+      highlight_color = result?.settings?.highlight_color || "#CCFFCC";
+      stop_color = result?.settings?.stop_color || "#555555";
+      stop_option = result?.settings?.stop_option || "collapse";
+      ad_option = result?.settings?.ad_option || "collapse";
+      recommend_option = result?.settings?.recommend_option || "collapse";
+
       setTimeout(() => main(), initial_hide_delay);
     });
 
--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ function append_keyword(kw){
   });
   new_div.append(new_delete_btn);
 
-  document.getElementById('keywords-container').append(new_div);
+  document.getElementById('keywords-list-container').append(new_div);
   new_textinput.addEventListener("keyup", ({key}) => {
     if (key === "Enter") {
        //let arr = [];
@@ -26,6 +26,33 @@ function append_keyword(kw){
        //keyword_inputs.forEach(e => {arr.push(e.value.length)});
        //console.log(arr);
        append_keyword("");
+    }
+})
+  new_textinput.focus();
+}
+
+function append_stopword(sw){
+  const new_div=document.createElement("div");
+  new_div.className="k";
+  
+  const new_textinput = document.createElement("input");
+  new_textinput.className = "stopwords";
+  new_textinput.value = sw;
+  new_textinput.placeholder = "insert a keyword here..."
+  new_div.append(new_textinput);
+
+  const new_delete_btn = document.createElement("button")
+  new_delete_btn.textContent = "remove";
+  new_delete_btn.addEventListener('click', (new_delete_btn) => {
+    new_div.remove();
+  });
+  new_div.append(new_delete_btn);
+
+  document.getElementById('stopwords-list-container').append(new_div);
+  new_textinput.addEventListener("keyup", ({key}) => {
+    if (key === "Enter") {
+       //let arr = [];
+       append_stopword("");
     }
 })
   new_textinput.focus();
@@ -46,49 +73,66 @@ chrome.storage.sync.get(['settings'], function(result) {
   console.log('Settings:', result.settings);
   if(result.settings){
     keywords = result.settings.keywords;
-    if(!keywords || keywords?.length==0){
-      hide_settings = false;
-    } else
-    keywords.forEach((k) => {
-      append_keyword(k);
-    });
+    if(keywords && keywords?.length > 0) keywords.forEach((k) => {append_keyword(k)});
+    stopwords = result.settings.stopwords;
+    if(stopwords && stopwords?.length > 0) stopwords.forEach((s) => {append_stopword(s)});
     hide_delay = result.settings.hide_delay;
     document.getElementById("hide-delay").value = hide_delay;
     highlight_color = result.settings.highlight_color;
+    stop_color = result.settings.stop_color;
+    stop_option = result.settings.stop_option || "collapse";
+    ad_option = result.settings.ad_option || "collapse";
+    recommend_option = result.settings.recommend_option || "collapse";
     document.getElementById("highlight-color").value = highlight_color;
     document.getElementById("highlight-example").style.backgroundColor = highlight_color;
+    document.getElementById("stop-color").value = stop_color;
+    document.getElementById("stop-example").style.backgroundColor = stop_color;
+    document.getElementById("stop-selector").value = stop_option;
+    document.getElementById("ad-selector").value = ad_option;
+    document.getElementById("recommend-selector").value = recommend_option;
       
   }
-  
   append_keyword("");
+  append_stopword("");
 });
 
 document.getElementById('apply').addEventListener('click', () => {
-  let keyWords=[];
+  let keyWords = [];
   const divCollection = document.getElementsByClassName("keywords");
   divArray = Array.from(divCollection);
   divArray.forEach((e) => {
-    if(e.value){
-      keyWords.push(e.value)
-    };
+    if(e.value) keyWords.push(e.value);
+  });
+  let stopWords = [];
+  Array.from(document.getElementsByClassName("stopwords")).forEach((e) => {
+    if(e.value) stopWords.push(e.value);
   });
   initial_hide_delay = document.getElementById("initial-hide-delay").value;
   hide_delay = document.getElementById("hide-delay").value;
   highlight_color = document.getElementById("highlight-color").value;
+  stop_color = document.getElementById("stop-color").value;
+  stop_option = document.getElementById("stop-selector").value;
+  ad_option = document.getElementById("ad-selector").value;
+  recommend_option = document.getElementById("recommend-selector").value;
   settings = {
     "initial_hide_delay":initial_hide_delay,
     "hide_delay":hide_delay,
     "highlight_color":highlight_color,
-    "keywords":keyWords
+    "keywords":keyWords,
+    "stop_color":stop_color,
+    "stopwords":stopWords,
+    "stop_option":stop_option,
+    "ad_option":ad_option,
+    "recommend_option":recommend_option
   }
   chrome.storage.sync.set({"settings": settings}, function() {
     status_message('Settings saved!');
     console.log('Settings saved');
   });
   document.getElementById("highlight-example").style.backgroundColor = highlight_color;
+  document.getElementById("stop-example").style.backgroundColor = stop_color;
   console.log(settings);
   document.getElementById('status').textContent = 'Done!';
-  //console.log('result: ', keyWords);
 });
 
 document.getElementById('reset').addEventListener('click', () => {
@@ -97,6 +141,10 @@ document.getElementById('reset').addEventListener('click', () => {
   console.log('Keywords reset');
 });
 
-document.getElementById('new').addEventListener('click', () => {
+document.getElementById('new-keyword').addEventListener('click', () => {
   append_keyword("");
+});
+
+document.getElementById('new-stopword').addEventListener('click', () => {
+  append_stopword("");
 });
